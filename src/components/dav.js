6d9ee351b829e2d11a3abbde08f1ac1e435de2d5
Naveen <naveen.chaurasia861@gmail.com>
2022-04-15 15:06:52 +0530

    added node visualisation 1

--- src/components/dav.js
+++ src/components/dav.js
@@ -3,13 +3,16 @@ import ForceGraph3D from "react-force-graph-3d";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 export default function Dav() {
-  const [Da, setDa] = useState([]);
+  const [Nodes1, setNodes1] = useState([]);
+  const [Nodes2, setNodes2] = useState([]);
+  const [Nodes, setNodes] = useState([]);
   const [NodeR, setNodeR] = useState([]);
+  const [Node3, setNode3] = useState([]);
   const body = {
     statements: [
       {
-        statement:
-          "match (n:Category) match (p:Products) MERGE (p)-[x:Product_of]->(c) RETURN n,p,x",
+        statement://"match (p:Products) match(pc:ProductComponents) MERGE (pc)-[y:component_of]->(p) RETURN p,pc,y LIMIT 10",
+       "match (n:Category) match (p:Products) MERGE (p)-[x:Product_of]->(c) RETURN n,p,x",
         resultDataContents: ["row", "graph"],
       },
     ],
@@ -32,21 +35,49 @@ export default function Dav() {
         );
         console.log(response.data.results[0].data[0].graph.nodes[0]);
         console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-        setDa(response.data.results[0].data[0].graph.nodes);
-        setNodeR(response.data.results[0].data[0].graph.relationships);
+
+      setNodes1 ( response.data.results[0].data.slice(0, 5).map((i)=>({nodes:i.graph.nodes[0]})));
+      setNodes2 ( response.data.results[0].data.slice(0, 5).map((i)=>({nodes:i.graph.nodes[1]})));
+      setNodes( [...Nodes1, ...Nodes2]);
+      
+      setNodeR( response.data.results[0].data.slice(0, 5).map((i)=>({nodesr:i.graph.relationships[0]})));
+      setNode3(NodeR.concat(Nodes1));
+      console.log("**********************");
+      console.log(response.data.results[0].data.slice(0, 5));
+      console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+      //  setNodes(response.data.results[0].data[0].graph.nodes);
+        //setNodeR(response.data.results[0].data[0].graph.relationships);
+     //debugger;
       });
   }, []);
 
   const gData = {
     //nodes: Da.map((node, id) => ({ id, node })),
-    nodes: Da.map((noder) => ({id: noder.id })),
+    nodes: [...Nodes1, ...Nodes2].map((noder) => ({id: noder.nodes.id })),
     links: NodeR.filter((noder) => noder).map((noder) => ({
-      source: noder.startNode,
-      target: noder.endNode,
+      source: noder.nodesr.startNode,
+      target: noder.nodesr.endNode,
     })),
   };
 
-  // const gData = {
+ 
+
+  return (
+    <div>
+      <ForceGraph3D
+        graphData={gData}
+        nodeLabel="id"
+        nodeAutoColorBy="group"
+        linkDirectionalParticles="value"
+        linkDirectionalParticleSpeed={(d) => d.value * 0.001}
+      />
+    </div>
+  );
+}
+
+
+
+ // const gData = {
   //   nodes: Da.nodes,
   //   links: Da.relationships
   // };
@@ -73,19 +104,6 @@ export default function Dav() {
   //       target: Math.round(Math.random() * (id-1))
   //     }))
   // };
-
-  return (
-    <div>
-      <ForceGraph3D
-        graphData={gData}
-        nodeLabel="id"
-        nodeAutoColorBy="group"
-        linkDirectionalParticles="value"
-        linkDirectionalParticleSpeed={(d) => d.value * 0.001}
-      />
-    </div>
-  );
-}
 
 // const genRandomTree = (N = 300) => {
 //   return {
@@ -133,3 +151,5 @@ export default function Dav() {
 //       target: Math.round(Math.random() * (id-1))
 //     }))
 // };
+// "match (p:Products) match(pc:ProductComponents) MERGE (pc)-[y:component_of]->(p) RETURN p,pc,y LIMIT 5",
+// "match (n:Category) match (p:Products) match(pc:ProductComponents) MERGE (p)-[x:Product_of]->(c) MERGE (pc)-[y:component_of]->(p) RETURN n,p,pc,x,y LIMIT 10",
